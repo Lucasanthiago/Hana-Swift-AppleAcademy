4c8930820f0d48f79adcd379f0c715e35cca4a3b
Lucasanthiago <santoslblucas@gmail.com>
2024-04-29 14:53:49 -0300

    Initial Commit

---
+++ PlanTio/PlanTioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanTioApp.swift
+//  PlanTio
+//
+//  Created by Lucas Santos on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanTioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
