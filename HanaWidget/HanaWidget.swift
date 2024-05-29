2a230d318c0f8feb6b036c884b8b2f0d38273b7b
Gabriela Azulay Lewin <gabialewin@gmail.com>
2024-05-29 18:13:55 -0300

    widgets

--- HanaWidget/HanaWidget.swift
+++ HanaWidget/HanaWidget.swift
@@ -1,13 +1,45 @@
 //
-//  WidgetPlantView.swift
-//  PlanTio
+//  HanaWidget.swift
+//  HanaWidget
 //
-//  Created by Gabriela Azulay Lewin on 28/05/24.
+//  Created by Gabriela Azulay Lewin on 29/05/24.
 //
 
+import WidgetKit
 import SwiftUI
 
-struct WidgetPlantView: View {
+struct Provider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    }
+
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: configuration)
+    }
+    
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        return Timeline(entries: entries, policy: .atEnd)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationAppIntent
+}
+
+struct HanaWidgetEntryView: View {
+//    var entry: Provider.Entry
+    
     let imageHana: String
     let text: String
     let sky: String
@@ -96,33 +128,35 @@ struct WidgetPlantView: View {
     }
 
 
-#Preview {
-    VStack {
-        WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "MorningClouds")
-            .background(.white)
-            .clipShape(.rect(cornerRadius: 21))
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.black)
-        
-        
-        WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "AfternoonSky")
-            .background(.white)
-            .clipShape(.rect(cornerRadius: 21))
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.black)
-        
-        WidgetPlantView(imageHana: "HanaSpring", text: "Have you checked your plants today?", sky: "EveningClouds")
-            .background(.white)
-            .clipShape(.rect(cornerRadius: 21))
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.black)
-       
-        WidgetPlantView(imageHana: "HanaSpringSleeping", text: "See you again tomorrow!", sky: "NightSky")
-            .background(.white)
-            .clipShape(.rect(cornerRadius: 21))
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.black)
+struct HanaWidget: Widget {
+    let kind: String = "HanaWidget"
+
+    var body: some WidgetConfiguration {
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+            HanaWidgetEntryView(imageHana: "HanaSpring", text: "Have you checked your plants today?", sky: "EveningClouds")
+                .containerBackground(.black, for: .widget)
+        }
+        .contentMarginsDisabled()
     }
 }
 
+extension ConfigurationAppIntent {
+    fileprivate static var smiley: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "😀"
+        return intent
+    }
+    
+    fileprivate static var starEyes: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "🤩"
+        return intent
+    }
+}
 
+#Preview(as: .systemMedium) {
+    HanaWidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
