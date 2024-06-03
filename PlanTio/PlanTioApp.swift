ae4bc6eca955a80bae95b1e1742140350ae846ee
kauats <kauat.santos12@gmail.com>
2024-06-03 15:11:22 -0300

    A versão melhorzinha

--- PlanTio/PlanTioApp.swift
+++ PlanTio/PlanTioApp.swift
@@ -20,23 +20,28 @@ struct PlanTioApp: App {
         PostHogSDK.shared.setup(config)
         
     }
-    
-    var body: some Scene {
-        WindowGroup {
-            //            TabBarView()
-            WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "MorningClouds")
-                .frame(width: 338, height: 158)
-            
-            WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "AfternoonSky")
-                .frame(width: 338, height: 158)
-            
-            WidgetPlantView(imageHana: "HanaSpring", text: "Have you checked your plants today?", sky: "EveningClouds")
-                .frame(width: 338, height: 158)
-            
-            WidgetPlantView(imageHana: "HanaSpringSleeping", text: "See you again tomorrow!", sky: "NightSky")
-                .frame(width: 338, height: 158)
+    var body : some Scene {
+        WindowGroup{
+            TabBarView()
         }
+        
     }
+//    var body: some Scene {
+//        WindowGroup {
+//            //            TabBarView()
+//            WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "MorningClouds")
+//                .frame(width: 338, height: 158)
+//            
+//            WidgetPlantView(imageHana: "HanaSpring", text: "Time to water your plants!", sky: "AfternoonSky")
+//                .frame(width: 338, height: 158)
+//            
+//            WidgetPlantView(imageHana: "HanaSpring", text: "Have you checked your plants today?", sky: "EveningClouds")
+//                .frame(width: 338, height: 158)
+//            
+//            WidgetPlantView(imageHana: "HanaSpringSleeping", text: "See you again tomorrow!", sky: "NightSky")
+//                .frame(width: 338, height: 158)
+//        }
+//    }
     
     
     //testando commit
