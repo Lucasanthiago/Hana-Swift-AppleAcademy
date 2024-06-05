9e0beeddebcaa8616550479502550fe4c50420ba
Lucasanthiago <santoslblucas@gmail.com>
2024-06-05 15:18:29 -0300

    add sheets do limit and max

--- PlanTio/Telas/ContentView.swift
+++ PlanTio/Telas/ContentView.swift
@@ -48,10 +48,9 @@ struct ContentView: View {
             .navigationBarTitle("My Plants")
             .navigationBarItems(
                 trailing:
-                    
                     HStack{
-                        
                         Button(action: {
+                            // Your upgrade action here
                             
                         }, label: {
                             Text("upgrade")
@@ -90,8 +89,6 @@ struct ContentView: View {
                                 .bold()
                         }
                     }
-                
-                
             )
             .navigationDestination(for: Plant.self) { plant in
                 if let index = viewModel.plants.firstIndex(where: { $0.id == plant.id }) {
@@ -106,12 +103,12 @@ struct ContentView: View {
                 PostHogSDK.shared.capture("searchUsed")
             })
             .navigationDestination(isPresented: $showingAddPlant) {
-                AddPlantView(viewModel: viewModel)
+                            AddPlantView(viewModel: viewModel)
             }
-            .navigationDestination(isPresented: $navigateToLimitReached) {
+            .sheet(isPresented: $navigateToLimitReached) {
                 LimitReachedView(viewModel: viewModel)
             }
-            .navigationDestination(isPresented: $navigateToMaxLimitReached) {
+            .sheet(isPresented: $navigateToMaxLimitReached) {
                 MaxLimitReachedView()
             }
         }
@@ -144,164 +141,3 @@ struct ContentView: View {
 #Preview {
     ContentView(viewModel: PlantViewModel())
 }
-
-
-
-struct MaxLimitReachedView: View {
-    var body: some View {
-        ZStack {
-            LinearGradient(
-                stops: [
-                    Gradient.Stop(color: .skyGradient1, location: 0),
-                    Gradient.Stop(color: .skyGradient2, location: 0.3)
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .ignoresSafeArea()
-            
-            Image("SunClouds")
-                .resizable()
-                .scaledToFit()
-                .frame(width:210)
-                .padding(.bottom, 580)
-                .padding(.leading, 100)
-            
-            ZStack {
-                RoundedRectangle(cornerRadius: 20.0)
-                    .foregroundStyle(
-                        LinearGradient(
-                            stops: [
-                                Gradient.Stop(color: .grassGradient1, location: 0),
-                                Gradient.Stop(color: .grassGradient2, location: 1)
-                            ],
-                            startPoint: .top,
-                            endPoint: .bottom
-                        )
-                    )
-                    .overlay(alignment: .top) {
-                        ZStack (alignment: .bottom){
-                            
-                            Image("Grass")
-                            
-                            Image("SadHana")
-                                .background(alignment: .bottom) {
-                                    Ellipse()
-                                        .frame(width: 120, height: 25.0)
-                                        .foregroundStyle(Color.blue.blendMode(.multiply).opacity(0.3))
-                                        .alignmentGuide(.bottom, computeValue: { _ in 18 })
-                                }
-                                .padding(.trailing, 180)
-                        }
-                        .alignmentGuide(.top, computeValue: {dimension in dimension[.bottom] - 20})
-                    }
-                    .ignoresSafeArea()
-                
-                VStack(alignment: .leading) {
-                    VStack {
-                        VStack(alignment: .leading, spacing: 25) {
-                            Text("Maximum Limit Reached")
-                                .font(.custom("Quicksand", size: 28, relativeTo: .title))
-                                .bold()
-                                .foregroundStyle(Color.darkGreen)
-                                .colorScheme(.light)
-                            
-                            
-                            Text("You have reached the maximum limit of 17 plants. Consider removing some plants to add new ones.")
-                                .font(.custom("Quicksand", size: 20, relativeTo: .title3))
-                                .foregroundStyle(Color.lightGreen)
-                                .fontWeight(.medium)
-                            
-                        }
-                        .padding(35)
-                        .background(
-                            RoundedRectangle(cornerRadius: 36)
-                                .foregroundStyle(Color.cards)
-                                .colorScheme(.light)
-                        )
-                        .padding(30)
-                        
-                        
-                        
-                        Button(action: {
-                            
-                        }, label: {
-                            Text("Go back to My Plants")
-                                .font(.custom("Quicksand", size: 22, relativeTo: .title2))
-                                .bold()
-                                .foregroundStyle(Color.white)
-                                .padding(20)
-                                .padding(.horizontal, 30)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 30)
-                                        .foregroundStyle(Color.pinkButton)
-                                        .colorScheme(.light)
-                                )
-                        })
-                        
-                    }
-                }
-            }
-            .padding(.top, 270)
-            
-            
-            
-            
-        }
-        
-    }
-    
-}
-        
-//
-//#Preview {
-//    MaxLimitReachedView()
-//}
-
-//struct MaxLimitReachedView: View {
-//    var body: some View {
-//        ZStack {
-//            Color("Background")
-//                .ignoresSafeArea()
-//            VStack {
-//                ZStack {
-//                    Rectangle()
-//                        .fill(Color("Cards"))
-//                        .frame(width: 400, height: 300)
-//                        .cornerRadius(10)
-//
-//                    HStack {
-//                        VStack(alignment: .leading) {
-//                            Text("Maximum Limit Reached")
-//                                .font(.custom("Quicksand", size: 25))
-//                                .bold()
-//                                .padding(.bottom, 15)
-//
-//                            Text("You have reached the maximum limit of 17 plants.")
-//                                .font(.custom("Quicksand", size: 15))
-//                                .padding(.bottom, 5)
-//                            Text("Consider removing some plants to add new ones.")
-//                                .font(.custom("Quicksand", size: 15))
-//                                .padding(.bottom, 2)
-//                        }
-//                        .padding()
-//                        Spacer()
-//
-//                        Image(systemName: "exclamationmark.triangle")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 50, height: 50)
-//                            .foregroundColor(.yellow)
-//                    }
-//                    .frame(width: 380)
-//                }
-//            }
-//            .navigationTitle("Limit Reached")
-//        }
-//        .background(Color("Background").ignoresSafeArea())
-//    }
-//}
-//
-//#Preview {
-//    MaxLimitReachedView()
-//}
