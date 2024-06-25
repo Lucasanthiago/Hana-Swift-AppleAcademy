1c7b29bbdc31d19048c4e3d6b6aed81506b034d5
Gabriela Azulay Lewin <gabialewin@gmail.com>
2024-06-25 14:47:07 -0300

    background hana plus working in all views

--- PlanTio/Telas/SunlightView.swift
+++ PlanTio/Telas/SunlightView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct SunlightView: View {
+    @EnvironmentObject var store: Store
     @ObservedObject var viewModel: PlantViewModel
     @State private var showingAddPlant = false
     @State var searchText = ""
@@ -16,7 +17,31 @@ struct SunlightView: View {
             }
             .font(.custom("Quicksand", size: 17, relativeTo: .body))
             .listStyle(PlainListStyle())
-            .background(Color("Background"))
+            .background{
+                if !store.hasPurchasedHanaPlus {
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: .background, location: 0)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                    .ignoresSafeArea()
+
+                }
+                else{
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: .hanaPlusGradient1, location: -0.3),
+                            Gradient.Stop(color: .hanaPlusGradient2, location: 0.56),
+                            Gradient.Stop(color: .hanaPlusGradient3, location: 1.4)
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .ignoresSafeArea()
+                }
+            }
             .navigationBarTitle("Sunbathing")
             .background(
                 NavigationLink(destination: AddPlantView(viewModel: viewModel), isActive: $showingAddPlant) {
