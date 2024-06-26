5a9585f990d9f978ec6ee74ed7831e2e9ae03a48
izabour <izabourb@gmail.com>
2024-06-25 22:40:53 -0300

    New Cards

--- PlanTio/Telas/Components/Instructions.swift
+++ PlanTio/Telas/Components/Instructions.swift
@@ -4,58 +4,58 @@
 //
 //  Created by izabour Azevedo on 24/06/24.
 //
-
 import SwiftUI
 
-struct Instructions<Content: View>: View {
+struct Instructions: View {
     
-    @ViewBuilder let content: Content
-    var title : String
-    var icon : String
-    var iconColor : Color
-    
+    @State var contentText: String
+    var title: String
+    var icon: String
+    var iconColor: Color
     
     var body: some View {
-        VStack {
-            VStack {
-                HStack{
-                    Image(systemName: icon)
-                        .padding(.leading)
-                        .foregroundStyle(iconColor)
-                        .font(.custom("", size: 60).bold())
-                    
-                    Text(title)
-                        .font(.custom("Quicksand", size: 20, relativeTo: .title3))
+        HStack {
+            Image(systemName: icon)
+                .foregroundStyle(iconColor)
+                .font(.system(size: 60).bold())
+            
+            VStack(alignment: .leading, spacing: 5) {
+                if contentText.isEmpty /*== true*/ {
+                    Text(title) // esse empty deve ser sinalizado também na datailView
+                        .font(.custom("Quicksand",
+                                      size: 20, relativeTo: .title3))
                         .bold()
                     
-                    Spacer()
-                        .environment(\.colorScheme, .dark)
+                } else {
+                    Text(title)
+                        .font(.custom("Quicksand",
+                                      size: 20, relativeTo: .title3))
+                        .bold()
                     
-                    
+                    Text(contentText)
+                        .font(.custom("Quicksand",
+                                      size: 15, relativeTo: .footnote))
                 }
             }
-            
-            content
         }
         .padding()
         .background(
             RoundedRectangle(cornerRadius: 20)
                 .foregroundStyle(Color("Cards"))
-                .shadow(color: .shadow.opacity(0.3), radius: 5, x: 0, y: 4)
+                .shadow(color: Color.black.opacity(0.3),
+                        radius: 5, x: 0, y: 4)
         )
-
     }
-    
-    
 }
 
 #Preview {
-    Instructions(content: {
-        VStack { Text("They usually need to be used once a week to allow the soil to dry out between waterings ")
-                .font(.custom("Quicksand", size: 15 , relativeTo: .footnote ))
-                .frame(maxWidth: .infinity)
-        } }, title: "Watering",
-                 icon: "drop.circle.fill",
-                 iconColor: .cyan)
-    .padding()
+    VStack {
+        Instructions(contentText: "", title: "Watering", icon: "drop.circle.fill", iconColor: .water) //Ex de como vai ficar vazio dps é só sinalizar
+        
+        Instructions(contentText: "Even though they're diurnal, orchids benefit from the night for respiration and energy storage.", title: "Sunbathing", icon: "sun.max.fill", iconColor: .sun)
+        
+        Instructions(contentText: "They go well with pets, but it is advisable to avoid eating them to stop digestive problems.", title: "Safe for Pets", icon: "pawprint.circle.fill", iconColor: .pinkButton)
+        
+        Instructions(contentText: "Thrive in a blend of bark, wood coal and moss, ensuring optimal drainage and root health.", title: "Best soil", icon: "leaf.circle.fill", iconColor: .soil)
+    }
 }
