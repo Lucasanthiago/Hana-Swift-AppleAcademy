0c254d7c3c23d0cf61dd11a472e719bc031b4b3e
izabour <izabourb@gmail.com>
2024-06-26 18:04:28 -0300

    New PlantDetailView

---
+++ PlanTio/Telas/Components/AdditionalCards.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionalCards.swift
+//  PlanTio
+//
+//  Created by izabour Azevedo on 25/06/24.
+//
+
+import SwiftUI
+
+struct AdditionalCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionalCards()
+}
